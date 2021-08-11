95465bbd49b485f0e6eeb9a765a5d498c29a7489
David Matos Farias <davidnfarias@gmail.com>
2021-08-11 16:45:52 -0300

    implemented context, service and feed page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,33 @@
+import { useContext } from 'react'
+import { AuthContext } from '../contexts/AuthContext'
+import { supabase } from '../services/supabase'
+
 export default function Home() {
+
+  const { user } = useContext(AuthContext)
+
+  console.log(user)
+
+  async function login() {
+    const { error } = await supabase.auth.signIn({
+      provider: 'github'
+    })
+
+    if (error) {
+      console.log(error)
+      return
+    }
+  }
+
+  async function logout() {
+    await supabase.auth.signOut()
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+    </div>
   )
+
 }
